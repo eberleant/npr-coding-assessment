1e9baad9968c7c41135ed829b8fa9c414b3d245a
Evalyn Berleant <eberleant@gmail.com>
2021-03-06 15:22:07 -0600

    Add tests; restrict length of local part with lookahead

--- src/main/java/org/npr/email_validation/EmailValidator.java
+++ src/main/java/org/npr/email_validation/EmailValidator.java
@@ -4,7 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmailValidator {
-    private static Pattern pattern = Pattern.compile("([\\w!#$%&'*+\\-/=?^`{|}~]+\\.?)*[\\w!#$%&'*+\\-/=?^_`{|}~]+@((([A-Za-z0-9]+-?)*[A-Za-z0-9]+)\\.)*(([A-Za-z0-9]+-?)*[A-Za-z0-9]+)");
+    // https://en.wikipedia.org/wiki/Email_address#Syntax
+    private static final Pattern pattern = Pattern.compile("^(?=.{1,64}@)([\\w!#$%&'*+\\-/=?^`{|}~]+\\.?)*[\\w!#$%&'*+\\-/=?^_`{|}~]+@((([A-Za-z0-9]+-?)*[A-Za-z0-9]+)\\.)*(([A-Za-z0-9]+-?)*[A-Za-z0-9]+)");
 
     public static boolean isValid(String email) {
         Matcher matcher = pattern.matcher(email);
--- src/test/java/org/npr/email_validation/EmailValidatorTest.java
+++ src/test/java/org/npr/email_validation/EmailValidatorTest.java
@@ -11,7 +11,59 @@ class EmailValidatorTest {
     }
 
     @Test
-    public void testBasic() {
-        assertTrue(EmailValidator.isValid("abc@abc.com"));
+    public void testBasicValid() {
+        // from https://en.wikipedia.org/wiki/Email_address#Examples (excluding quoted local parts)
+        assertTrue(EmailValidator.isValid("simple@example.com"));
+        assertTrue(EmailValidator.isValid("very.common@example.com"));
+        assertTrue(EmailValidator.isValid("disposable.style.email.with+symbol@example.com"));
+        assertTrue(EmailValidator.isValid("other.email-with-hyphen@example.com"));
+        assertTrue(EmailValidator.isValid("fully-qualified-domain@example.com"));
+        assertTrue(EmailValidator.isValid("user.name+tag+sorting@example.com"));
+        assertTrue(EmailValidator.isValid("x@example.com"));
+        assertTrue(EmailValidator.isValid("example-indeed@strange-example.com"));
+        assertTrue(EmailValidator.isValid("admin@mailserver1"));
+        assertTrue(EmailValidator.isValid("example@s.example"));
+        assertTrue(EmailValidator.isValid("mailhost!username@example.org"));
+        assertTrue(EmailValidator.isValid("user%example.com@example.org"));
+        assertTrue(EmailValidator.isValid("user-@example.org"));
+    }
+
+    @Test
+    public void testBasicInvalid() {
+        // from https://en.wikipedia.org/wiki/Email_address#Examples
+        assertFalse(EmailValidator.isValid("Abc.example.com"));
+        assertFalse(EmailValidator.isValid("A@b@c@example.com"));
+        assertFalse(EmailValidator.isValid("a\"b(c)d,e:f;g<h>i[j\\k]l@example.com"));
+        assertFalse(EmailValidator.isValid("just\"not\"right@example.com"));
+        assertFalse(EmailValidator.isValid("this is\"not\\allowed@example.com"));
+        assertFalse(EmailValidator.isValid("this\\ still\\\"not\\\\allowed@example.com"));
+        assertFalse(EmailValidator.isValid("1234567890123456789012345678901234567890123456789012345678901234+x@example.com"));
+        assertFalse(EmailValidator.isValid("i_like_underscore@but_its_not_allowed_in_this_part.example.com"));
+    }
+
+    @Test
+    public void testLocalPartValid() {
+        assertTrue(EmailValidator.isValid("!#$%&'*+-/=?^_`{|}~0123456789abczABCZ@abc.com"));
+        assertTrue(EmailValidator.isValid("a@abc.com"));
+        assertTrue(EmailValidator.isValid("a.b.c.e@abc.com"));
+        assertTrue(EmailValidator.isValid("abc.d.ef@abc.com"));
+    }
+
+    @Test
+    public void testLocalPartInvalidPeriod() {
+        assertFalse(EmailValidator.isValid(".abc@abc.com"));
+        assertFalse(EmailValidator.isValid("abc.@abc.com"));
+        assertFalse(EmailValidator.isValid("abc..abc@abc.com"));
+        assertFalse(EmailValidator.isValid(".@abc.com"));
+    }
+
+    @Test
+    public void testLocalPartEmpty() {
+        assertFalse(EmailValidator.isValid("@abc.com"));
+    }
+
+    @Test
+    public void testDomainValid() {
+
     }
 }
